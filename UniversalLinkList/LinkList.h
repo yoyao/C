eacd51a0dee6f0f6be5b3e5aace631e11def5a13
YoyaoFeng <fengweixiang0912@gmail.com>
2017-01-05 17:33:21 +0800

    通用链表
    你包含不了世界，就要让世界包含你。

---
+++ UniversalLinkList/LinkList.h
@@ -0,0 +1,39 @@
+
+
+#ifndef _LINKLIST_H_
+
+#define _LINKLIST_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef struct listnode ListNode;
+
+struct listnode{
+	ListNode* PrevNode;//上一个节点
+	ListNode* NextNode;//下一个节点
+};
+
+typedef struct link_list LinkList;
+
+struct link_list{
+	unsigned int Count;//子节点数量
+	ListNode* HeadNode;//头节点
+	ListNode* LastNode;//尾节点
+};
+ListNode* CreatNode();//创建节点，返回指针
+
+void InitLinkList(LinkList* plinklist);//初始化链表，将链表清空
+
+unsigned int ClearListNode(ListNode* HeadNode);//清空给定节点的子节点
+
+unsigned int GetListCount();//获取LinkList的节点数量
+
+int InsertListNodeOfPosi(LinkList* plinklist,const unsigned int posi,ListNode* p_node);//在指定位置处插入ListNode
+
+int GetListNodeOfPosi(LinkList* plinklist,const unsigned int posi);//获取指定位置的ListNode
+
+int PushBack(LinkList* plinklist,ListNode* p_node);//在链表最后添加Node
+
+#endif
